c220555eb143cd69310154674a17bbd305911ab2
zhaoyl <1486607544@qq.com>
2020-04-03 10:29:00 +0800

    sadf

--- bat.sh
+++ bat.sh
@@ -1,4 +1,5 @@
 cnpm install
 npm run build
+docker stop  `docker ps -aq --filter ancestor=jenkins_nginx_vue_demo:v1`
 docker build -t jenkins_nginx_vue_demo:v1  .
 docker run -d -p 8090:80 jenkins_nginx_vue_demo:v1
