1bd300e4c4dc9351e3eb9c4e9c218cc8e81b2f25
zhaoyl <1486607544@qq.com>
2020-04-03 14:11:21 +0800

    ads

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-      4444111
+      4444111222
     </div>
     <router-view />
   </div>
