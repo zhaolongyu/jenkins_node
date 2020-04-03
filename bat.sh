147f3b74a2e6f5ae30ecf4134a920b49e6ecd0fa
zhaoyl <1486607544@qq.com>
2020-04-03 11:52:08 +0800

    sdf

--- bat.sh
+++ bat.sh
@@ -1,8 +1,19 @@
 cnpm install
 npm run build
-if  `docker ps -aq --filter ancestor=jenkins_nginx_vue_demo:v1`;then 
- docker stop  `docker ps -aq --filter ancestor=jenkins_nginx_vue_demo:v1`
+
+docker ps | grep jenkins_nginx_vue_demo &> /dev/null
+if [ $? -eq 0 ]
+then
+   docker stop  `docker ps -aq --filter ancestor=jenkins_nginx_vue_demo:v1`
 fi
+
+docker images | grep jenkins_nginx_vue_demo &> /dev/null
+if [ $? -eq 0 ]
+then
+   docker image rm -f  jenkins_nginx_vue_demo
+fi
+
+
 docker build -t  jenkins_nginx_vue_demo:v1  .
 docker run -d -p 8090:80   jenkins_nginx_vue_demo:v1
 npm -v
