1ae04ebdede1ed94f1007f389ad197e4c3207773
zhaoyl <1486607544@qq.com>
2020-04-03 14:17:35 +0800

    ads

--- bat.sh
+++ bat.sh
@@ -1,7 +1,7 @@
 cnpm install
 npm run build
 
-docker ps | grep jenkins_nginx_vue_demo &> /dev/null
+docker ps -a | grep jenkins_nginx_vue_demo &> /dev/null
 if [ $? -eq 0 ]
 then
    npm -v
@@ -12,7 +12,7 @@ docker images | grep jenkins_nginx_vue_demo &> /dev/null
 if [ $? -eq 0 ]
 then
    node -v
-   docker rmi  -f  jenkins_nginx_vue_demo
+   docker rmi  -f  jenkins_nginx_vue_demo:v1
 fi
 
 
