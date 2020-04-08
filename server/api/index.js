532a4a2d65b2b10a633d09f31d350d764ab1b710
zhaoyl <1486607544@qq.com>
2020-04-08 17:22:03 +0800

    s

--- server/api/index.js
+++ server/api/index.js
@@ -2,9 +2,7 @@ const Router = require("express").Router();
 
 const login = require("./modules/login");
 const home = require("./modules/home");
-const submit = require("./modules/submit");
 Router.use("/", login);
 Router.use("/", home);
-Router.use("/", submit);
 
 module.exports = Router;
