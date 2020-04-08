bcf6a6f8430b520078c6fabcaa435192f3f72377
zhaoyl <1486607544@qq.com>
2020-04-08 14:49:55 +0800

    d

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import store from "./store";
 import axios from "./axios";
 // axios.defaults.baseURL = "/api/";
-axios.defaults.baseURL = "http://47.100.188.145:3001";
+axios.defaults.baseURL = "http://47.100.188.145:8090";
 Vue.prototype.axios = axios;
 new Vue({
   router,
