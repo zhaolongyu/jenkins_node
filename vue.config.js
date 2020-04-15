9ce7e34e8b02ae24ba007cbfd796d08497a9985b
zhaoyl <1486607544@qq.com>
2020-04-15 10:05:17 +0800

    s

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const path = require("path"); //引入path模块
+const UglifyJsPlugin =require('uglify-js-plugin')
 function resolve (dir) {
   return path.join(__dirname, dir); //path.join(__dirname)设置绝对路径
 }
